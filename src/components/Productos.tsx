92d459e4a37ae877df6c43bab647510c9f9e8c73
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-26 13:15:37 -0500

    componentes tsx

--- src/components/Productos.tsx
+++ src/components/Productos.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Productos = () => {
   return (
-    <>
-      <div>Desde Productos</div>
-    </>
+    <Fragment>
+      <h2 className="text-center my-5">Listado de Productos</h2>
+      <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </Fragment>
   );
 };
 
