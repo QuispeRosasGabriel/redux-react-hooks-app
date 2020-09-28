393e7a3b80b4114e26712a7ecac0433416325b08
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-27 19:23:35 -0500

    controlando excepciones

--- src/components/Productos.tsx
+++ src/components/Productos.tsx
@@ -16,11 +16,19 @@ const Productos = () => {
 
   // Obtener el state
   const productos = useSelector((state: any) => state.productos.productos);
-  console.log(productos);
+  const error = useSelector((state: any) => state.productos.error);
+  const cargando = useSelector((state: any) => state.productos.cargando);
 
   return (
     <Fragment>
       <h2 className="text-center my-5">Listado de Productos</h2>
+      {error && (
+        <p className="font-weight-bold alert alert-danger text-center mt-4">
+          Hubo un error...
+        </p>
+      )}
+
+      {cargando && <p className="text-center">Cargando...</p>}
       <table className="table table-striped">
         <thead className="bg-primary table-dark">
           <tr>
